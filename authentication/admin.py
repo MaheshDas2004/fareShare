e93f0b70d683a7c9285be34fa34db5901567396f
Mahesh Das <mahesh.das2205@gmail.com>
2026-06-13 18:58:40 +0530

    add custom user model

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Apne model ko import karein
+from .models import User
+
+# Model ko register karein
+admin.site.register(User)
--- config/settings.py
+++ config/settings.py
@@ -95,6 +95,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
+
+AUTH_USER_MODEL = "authentication.User"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
